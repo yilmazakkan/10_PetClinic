4e173fbff60a1e2536f51d217cf674251790e42d
Yilmaz Akkan <yilmaz.akkan@gmail.com>
2019-07-09 17:08:51 +0300

    REST Api eklenmesi

--- 10_PetClinic/src/main/java/com/yilmazakkan/petclinic/exception/OwnerNotFoundException.java
+++ 10_PetClinic/src/main/java/com/yilmazakkan/petclinic/exception/OwnerNotFoundException.java
@@ -2,9 +2,10 @@ package com.yilmazakkan.petclinic.exception;
 
 public class OwnerNotFoundException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public OwnerNotFoundException(String message) {
 		super(message);
-		
 	}
 
 }
